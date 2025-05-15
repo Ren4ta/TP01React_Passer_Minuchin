16cd237fdbec1f54bfb134e3c3439e34409f88b8
Ren4ta <48313480@est.ort.edu.ar>
2025-05-15 12:12:54 -0300

    confirmacion eleminar

--- tp01react/src/App.jsx
+++ tp01react/src/App.jsx
@@ -1,40 +1,49 @@
 import './App.css';
 import { useState } from 'react';
 import TarjetaCita from './components/tarjetaCita/tarjetaCita';
-import CrearCita from './components/crearCita/crearCita';
+import CrearCita from './components/crearCita/crearCita'; 
+
 
 function App() {
   const [citas, setCitas] = useState([]);
 
   const agregarCita = (nuevaCita) => {
-    setCitas([...citas, nuevaCita]);
+    const id = new Date().getTime(); 
+    setCitas([...citas, { ...nuevaCita, id }]);
   };
-
+  const eliminarCita = (id) => {
+    const confirm = window.confirm("¿Estás seguro de que deseas eliminar esta cita?");
+    if (confirm) {
+      const nuevasCitas = citas.filter((cita) => cita.id !== id);
+      setCitas(nuevasCitas);
+    }
+  };
   return (
     <>
-      <h1>Administrador de Pacientes</h1>
-      <div className="container">
-        <div className="row">
-          <div className="one-half column">
-            <h1>Crear mi cita</h1>
-            <CrearCita crearCita={agregarCita} />
-          </div>
-
-          <div className="one-half column">
-            <h1>Administra tus citas</h1>
-            {citas.map((cita, numeroCitas) => (
-              <TarjetaCita 
-                key={numeroCitas}
-                nombre={cita.nombre}
-                dueño={cita.dueño}
-                fecha={cita.fecha}
-                hora={cita.hora}
-                sintomas={cita.sintomas}
-              />
-            ))}
-          </div>
-        </div>
+<h1>Administrador de Pacientes</h1>
+<div className="container">
+  <div className="formulario">
+    <h2>Formulario</h2>
+    <CrearCita crearCita={agregarCita} />
+  </div>
+  
+  <div className="listado">
+    <h2>Listado</h2>
+    {citas.map((cita) => (
+      <div key={cita.id} className="cita">
+        <TarjetaCita 
+          id={cita.id}
+          nombre={cita.nombre}
+          dueño={cita.dueño}
+          fecha={cita.fecha}
+          hora={cita.hora}
+          sintomas={cita.sintomas} 
+          eliminarCita={eliminarCita}
+        />
       </div>
+    ))}
+  </div>
+</div>
     </>
   );
 }
--- tp01react/src/components/tarjetaCita/tarjetaCita.jsx
+++ tp01react/src/components/tarjetaCita/tarjetaCita.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import './tarjetaCita.css'
 
-const tarjetaCita = (props) => {
+const tarjetaCita = (props) => { 
+    const { id, nombre, dueño, fecha, hora, sintomas, eliminarCita } = props;
     return (
         <div class="cita">
             <p>Mascota: <span>{props.nombre}</span></p>
             <p>Dueño: <span>{props.dueño}</span></p>
             <p>Fecha: <span>{props.fecha}</span></p>
             <p>Hora: <span>{props.hora}</span></p>
-            <p>Sintomas: <span>{props.sintomas}</span></p><button className="button elimnar u-full-width">Eliminar ×</button>
+            <p>Sintomas: <span>{props.sintomas}</span></p>
+            <button 
+                className="button eliminar u-full-width" 
+                onClick={() => eliminarCita(id)} 
+            >Eliminar ×</button>
         </div>
     );
 }
